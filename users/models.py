b9423aca636f4edca2eb1e1b0ba4046f4e9fc783
Olga Semenova <helen597597@gmail.com>
2024-05-26 19:09:59 +0500

    edit

--- users/models.py
+++ users/models.py
@@ -8,14 +8,13 @@ class User(AbstractUser):
     username = None
     email = models.EmailField(unique=True, verbose_name='почта')
     phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
-    telegram = models.CharField(max_length=150, verbose_name='Telegram_id', **NULLABLE)
+    telegram_chat_id = models.CharField(max_length=50, unique=True, verbose_name='Telegram_chat_id', **NULLABLE)
     country = models.CharField(max_length=100, verbose_name='страна', **NULLABLE)
     city = models.CharField(max_length=100, verbose_name='город', **NULLABLE)
     avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
     verification_code = models.CharField(max_length=10, verbose_name='код верификации', **NULLABLE)
     last_login = models.DateTimeField(**NULLABLE, verbose_name='дата последнего входа')
     is_active = models.BooleanField(default=False, verbose_name='активен')
-    tg_chat_id = models.CharField(max_length=50, verbose_name='telegram chat id', **NULLABLE)
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []
 
