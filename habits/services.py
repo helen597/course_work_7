d24ba76e7c3a38b8e98e3557d45d5f8aedd0390e
Olga Semenova <helen597597@gmail.com>
2024-05-27 16:22:31 +0500

    edit

--- habits/services.py
+++ habits/services.py
@@ -4,8 +4,12 @@ from config import settings
 
 def send_telegram_message(chat_id, message):
     """Функция отправки уведомления в Telegram"""
-    params = {'chat_id': chat_id, 'text': message}
+    params = {
+        'chat_id': chat_id,
+        'text': message,
+    }
     print(f'{settings.TELEGRAM_URL}{settings.TELEGRAM_TOKEN}')
+    print(params)
     return requests.post(
         f'{settings.TELEGRAM_URL}{settings.TELEGRAM_TOKEN}/sendMessage',
         params=params
