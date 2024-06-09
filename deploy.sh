d2a05c149ae9f44f21f15aaac97dcb0936a6ee5f
Olga Semenova <helen597597@gmail.com>
2024-06-09 19:09:38 +0500

    add gitlab ci

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
-python3 -m venv venv
-source venv/bin/activate
-pip3 install -r requirements.txt
-python3 manage.py migrate
-python3 manage.py collectstatic --input
-deactivate
+#python3 -m venv venv
+#source venv/bin/activate
+#pip3 install -r requirements.txt
+#python3 manage.py migrate
+#python3 manage.py collectstatic --input
+#deactivate
+
+docker-compose up -d --build
